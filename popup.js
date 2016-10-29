506699e9013f613b97ac0b5e00563c97c7caa88b
wangliu <wangliu@isoftstone.com>
2016-10-29 21:11:16 +0800

    added option page.

--- popup.js
+++ popup.js
@@ -1,6 +1,13 @@
-$(function () {
-    $('#start').on('click', function () {
-        alert('ok');
+/**
+ *
+ * @summary ÷˜“≥√Ê ”Õº
+ * */
+function masterView() {
+    var self = this;
+    self.btnStart = function () {
         window.close();
-    });
-});
+        alert("It's works! Nice! ");
+    };
+}
+var mainView = new masterView();
+ko.applyBindings(mainView);
