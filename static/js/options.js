0ffc5fa40e1c72ee3ba1a42537e7576203fa1c95
wangliu <wangliu@isoftstone.com>
2016-10-30 22:10:06 +0800

    added default settings in option page.

--- static/js/options.js
+++ static/js/options.js
@@ -1,14 +1,101 @@
 /**
  *
+ * @summary 常规参数设置
+ * */
+function normalSettings(settings) {
+    var self = this;
+    //最大盈利
+    self.maxWinMoney = ko.observable(settings.maxWinMoney);
+    //最大亏损
+    self.maxLoseMoney = ko.observable(settings.maxLoseMoney);
+}
+
+/**
+ *
+ * @summary 自动关机参数设置
+ * */
+function autoShutDownSettings(settings) {
+    var self = this;
+    //自动关机 最大盈利
+    self.autoShutdownPC = ko.observable(settings.autoShutdownPC);
+}
+
+/**
+ *
+ * @summary 提示条
+ * */
+function promptToolBar() {
+    var self = this;
+    //是否显示 默认不显示
+    self.isShow = ko.observable(false);
+    //提示内容
+    self.promptText = ko.observable(null);
+}
+
+/**
+ *
  * @summary option页面视图
  * */
-function optionPage() {
+function optionPage(settings) {
     var self = this;
+    //提示条
+    self.promptToolBar = new promptToolBar();
+    //常规设置
+    self.normalSettings = new normalSettings(settings.normalSettings);
+    //自动关机设置
+    self.autoShutDownSettings = new autoShutDownSettings(settings.autoShutDownSettings);
+    //显示提示
+    self.showAlertMessage = function (text) {
+        var promptWindow = self.promptToolBar;
+        promptWindow.promptText(text);
+        promptWindow.isShow(true);
+        setTimeout(function () {
+            promptWindow.isShow(false);
+        }, 800);
+    };
     //保存设置
-    this.saveSettings = function () {
-        //TODO:添加保存参数代码
+    self.saveSettings = function () {
+        chrome.storage.sync.set({
+            normalSettings: {
+                maxWinMoney: self.normalSettings.maxWinMoney(),
+                maxLoseMoney: self.normalSettings.maxLoseMoney()
+            },
+            autoShutDownSettings: {
+                autoShutdownPC: self.autoShutDownSettings.autoShutdownPC()
+            }
+        }, function () {
+            //提示保存结果
+            self.showAlertMessage('保存成功！');
+        });
+    };
+    //恢复默认设置
+    self.restoreSettings = function () {
+        chrome.storage.sync.set(defaultSettings, function () {
+            self.showAlertMessage('恢复默认设置成功！请刷新页面！');
+        });
+    };
+    //取消
+    self.cancelSaveSettings = function () {
+        window.close();
     };
 }
 
-var pageView = new optionPage();
-ko.applyBindings(pageView);
+//默认设置
+var defaultSettings = {
+    normalSettings: {
+        maxWinMoney: null,
+        maxLoseMoney: null
+    },
+    autoShutDownSettings: {
+        autoShutdownPC: 'maxWinMoney'
+    }
+};
+
+$(function () {
+    //获取设置
+    chrome.storage.sync.get(null, function (optionSettings) {
+        var pageView = new optionPage($.extend({}, defaultSettings, optionSettings));
+        ko.applyBindings(pageView);
+    });
+
+});
