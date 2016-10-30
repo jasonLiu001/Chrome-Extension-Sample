dbf10f8e3b651aae20fe0c8ccd2a56ce272d6bc3
wangliu <wangliu@isoftstone.com>
2016-10-30 16:52:46 +0800

    init popup page.

--- static/js/popup.js
+++ static/js/popup.js
@@ -6,8 +6,12 @@ function popupPage() {
     var self = this;
     //开始按钮
     self.btnStart = function () {
-        //TODO:这里添加自己代码
+        //TODO:开始任务
         window.close();
+    };
+    //停止按钮
+    self.btnStop=function(){
+        //TODO:停止任务
     };
 }
 var mainView = new popupPage();
