5d6678f71046b06d94f8d054df31f0b46772157a
wangliu <wangliu@isoftstone.com>
2016-11-25 21:45:18 +0800

    代码结构调整，移除自动执行的方式来构建类

--- static/js/background.js
+++ static/js/background.js
@@ -6,8 +6,13 @@ chrome.storage.sync.get(null, function (optionSettings) {
     chrome.storage.sync.set($.extend({}, window.defaultSettings, optionSettings));
 });
 
-var automationObj = window.uiAutomator;
-//step1:设置平台为经纬平台
+/**
+ *
+ * @summary 初始化自动投注对象，初始化完成后，需要立即调用方法=>setPlatform
+ * setPlatform => 用来设置实际投注的平台，如果平台发生改变，需要改变该方法中调用的枚举值
+ * */
+var automationObj = new uiAutomator();
+//【必须】首先设置平台为经纬平台，或者为其他平台
 automationObj.setPlatform(enumPlatformList.jingwei);
 
 /**
@@ -15,7 +20,7 @@ automationObj.setPlatform(enumPlatformList.jingwei);
  * @summary popup页面 开始任务
  * */
 function btnStart() {
-    //step2:执行投注
+    //调用投注对象的执行投注
     automationObj.execInvest();
 }
 
--- static/js/sites/jingwei.js
+++ static/js/sites/jingwei.js
@@ -3,66 +3,58 @@
  *
  * @summary 经纬平台
  * */
-(function () {
-    /**
-     *
-     * @summary 经纬平台
-     * */
-    function jingweiplatform() {
-    }
+/**
+ *
+ * @summary 经纬平台
+ * */
+function jingweiplatform() {
+}
 
-    /**
-     *
-     * @summary 获取上期开奖期号
-     * @return {String} 期号 如：20161120-032
-     * */
-    jingweiplatform.prototype.getLastPeriodNumberString = function () {
-    };
+/**
+ *
+ * @summary 获取上期开奖期号
+ * @return {String} 期号 如：20161120-032
+ * */
+jingweiplatform.prototype.getLastPeriodNumberString = function () {
+};
 
-    /**
-     *
-     * @summary 获取上期开奖号码
-     * @return {String} 上期开奖号码 如：58903
-     * */
-    jingweiplatform.prototype.getLastPrizeNumberString = function () {
-    };
+/**
+ *
+ * @summary 获取上期开奖号码
+ * @return {String} 上期开奖号码 如：58903
+ * */
+jingweiplatform.prototype.getLastPrizeNumberString = function () {
+};
 
-    /**
-     *
-     * @summary 获取当前投注期号
-     * @return {String} 期号 如：20161120-032
-     * */
-    jingweiplatform.prototype.getCurrentPeriodNumber = function () {
-    };
+/**
+ *
+ * @summary 获取当前投注期号
+ * @return {String} 期号 如：20161120-032
+ * */
+jingweiplatform.prototype.getCurrentPeriodNumber = function () {
+};
 
-    /**
-     *
-     * @summary 执行投注
-     * */
-    jingweiplatform.prototype.execInvest = function () {
-        //需要中奖模块暴露出上期投注是否中奖，这样才能为下一步的盈利计算，提供必要的依据
-        //判定盈利是否已经到达最大，或者亏损到达最大，停止投注
-        //是否到了投注时间，到了则投注，没到时间则继续等待
-        //期号和当前要投的期号是否一致，一致投注，不一致则继续等待
-        //当前的投注号码获取模块
-        //TODO:执行UI自动投注
+/**
+ *
+ * @summary 执行投注 必须实现的接口方法
+ * */
+jingweiplatform.prototype.execInvest = function () {
+    //需要中奖模块暴露出上期投注是否中奖，这样才能为下一步的盈利计算，提供必要的依据
+    //判定盈利是否已经到达最大，或者亏损到达最大，停止投注
+    //是否到了投注时间，到了则投注，没到时间则继续等待
+    //期号和当前要投的期号是否一致，一致投注，不一致则继续等待
+    //当前的投注号码获取模块
+    //TODO:执行UI自动投注
 
 
-        alert('ok');
-    };
+    alert('ok');
+};
 
-    /**
-     *
-     * @summary 停止投注
-     * */
-    jingweiplatform.prototype.stopInvest = function () {
-        //TODO:这里添加测试代码
-        alert('ok');
-    };
-
-    if (window.jingweiplatform === undefined || window.jingweiplatform === null) {
-        window.jingweiplatform = new jingweiplatform();
-    } else {
-        return window.jingweiplatform;
-    }
-})();
+/**
+ *
+ * @summary 停止投注 必须实现的接口方法
+ * */
+jingweiplatform.prototype.stopInvest = function () {
+    //TODO:这里添加测试代码
+    alert('ok');
+};
