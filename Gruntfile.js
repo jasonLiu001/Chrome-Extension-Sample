6cdcbedead57db7dee364c15be534a31b6cbf04c
wangliu <wangliu@isoftstone.com>
2016-12-22 21:26:33 +0800

    use grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                banner: '/*! <%= pkg.name %> - <%= grunt.template.today("yyyy-mm-dd")%>  */'
+            },
+            dist: {
+                files: {
+                    'dist/autoInvest.debug.js': [
+                        "static/libs/moment.min.js",
+                        "static/js/global/globalVar.js",
+                        "static/js/enum/enumItem.js",
+                        "static/js/time/opentime.js",
+                        "static/js/number/numberFactory.js",
+                        "static/js/platform/jingwei.js",
+                        "static/js/platform/dreams.js",
+                        "static/js/appMain.js"
+                    ]
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - <%= grunt.template.today("yyyy-mm-dd")%>  */',
+                drop_console: true
+            },
+            dist: {
+                files: {
+                    'dist/autoInvest.min.js': ['dist/autoInvest.debug.js']
+                }
+            }
+        },
+        watch: {
+            build: {
+                files: ['*.js', './**/*.js'],
+                //tasks: ['concat', 'jshint', 'uglify'],//暂时移除语法检查
+                tasks: ['concat', 'uglify'],
+                options: {spawn: true}
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat'); //合并
+    //grunt.loadNpmTasks('grunt-contrib-jshint'); //语法检查
+    grunt.loadNpmTasks('grunt-contrib-uglify'); //压缩
+    grunt.loadNpmTasks('grunt-contrib-watch'); //文件修改监视
+    //任务执行 这里注意各个任务的执行顺序
+    grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+};
