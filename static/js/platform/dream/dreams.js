71404935a1ba62b0b427984346bca2b6cce3dbd3
wangliu <wangliu@isoftstone.com>
2016-12-25 14:11:11 +0800

    added new rules

--- static/js/platform/dream/dreams.js
+++ static/js/platform/dream/dreams.js
@@ -79,72 +79,7 @@ CityOfDreamsPlatForm.prototype.execInvest = function (investNumberString) {
  * 后二直选投注
  * */
 CityOfDreamsPlatForm.prototype.houErZhiXuanInvest = function (investNumberString) {
-    //后二
-    $(window.frames["mainFrame"].document).find('#Bet > table > tbody > tr:nth-child(2) > td > div:nth-child(1) > div.tabpanelBox > ul > li:nth-child(7) > a')[0].click();
-    //分模式
-    $(window.frames["mainFrame"].document).find('#Unit2c > label')[0].click();
-    //后二直选
-    $(window.frames["mainFrame"].document).find('#NumberPositionMatchForL2StarSingle')[0].click();
-    //点击清空号码
-    $(window.frames["mainFrame"].document).find('#text-form > td > div > div.col-md-8 > button:nth-child(2)')[0].click();
-    $(window.frames["mainFrame"].document).find('#betNumber-placeholder')[0].click();
-
-    //输入号码
-    $(window.frames["mainFrame"].document).find('#investNumbersContainer').text(23);
+    //输入号码 配合注入的脚本来完成自动投注，通过下面给页面中特定的节点来赋值
+    $(window.frames["mainFrame"].document).find('#investNumbersContainer').text(22);
     //因为该平台使用的是knockout加iframe的形式，所以号码投注的部分使用injectedcontent.js中方法来完成
-};
-
-/**
- *
- * 后一投注
- * */
-CityOfDreamsPlatForm.prototype.houYiInvest = function (investNumberString) {
-    //关闭提示框
-    $('body > div:nth-child(18) > div:nth-child(4) > span').click();
-    //定位胆
-    $("#pmrange > li:nth-child(9)").click();
-    //个位
-    $('#subpms > div > ul > li:nth-child(6)').click();
-    //选择分模式
-    $('#extra_area > div:nth-child(4) > ul > li:nth-child(4)').click();
-    //选号码
-    for (var i = 0; i < investNumberString.length; i++) {
-        var item = investNumberString[i];
-        switch (item) {
-            case '0':
-                $('#betarea > div > div > ul > li:nth-child(1)').click();
-                break;
-            case '1':
-                $('#betarea > div > div > ul > li:nth-child(2)').click();
-                break;
-            case '2':
-                $('#betarea > div > div > ul > li:nth-child(3)').click();
-                break;
-            case '3':
-                $('#betarea > div > div > ul > li:nth-child(4)').click();
-                break;
-            case '4':
-                $('#betarea > div > div > ul > li:nth-child(5)').click();
-                break;
-            case '5':
-                $('#betarea > div > div > ul > li:nth-child(6)').click();
-                break;
-            case '6':
-                $('#betarea > div > div > ul > li:nth-child(7)').click();
-                break;
-            case '7':
-                $('#betarea > div > div > ul > li:nth-child(8)').click();
-                break;
-            case '8':
-                $('#betarea > div > div > ul > li:nth-child(9)').click();
-                break;
-            case '9':
-                $('#betarea > div > div > ul > li:nth-child(10)').click();
-                break;
-        }
-    }
-    //添加到投注
-    $('#extra_area > div:nth-child(1) > img').click();
-    //确认投注
-    $('#bets_list_area > div > div:nth-child(1) > img').click();
 };
--- static/js/platform/dream/injectedcontent.js
+++ static/js/platform/dream/injectedcontent.js
@@ -1,16 +1,42 @@
 /**
  *
+ * @summary 存储投注号码容器
+ * */
+var investNumbersContainer = 'investNumbersContainer';
+
+/**
+ *
+ * @summary 后二投注
+ * */
+function houErZhiXuanInvest(investNumberString) {
+    //后二
+    $('#Bet > table > tbody > tr:nth-child(2) > td > div:nth-child(1) > div.tabpanelBox > ul > li:nth-child(7) > a')[0].click();
+    //分模式
+    $('#Unit2c > label')[0].click();
+    //后二直选
+    $('#NumberPositionMatchForL2StarSingle').click();
+    //点击清空号码
+    $('#text-form > td > div > div.col-md-8 > button:nth-child(2)').click();
+    $('#betNumber-placeholder').click();
+    //输入号码
+    viewModel.textForm(investNumberString);
+    //投注
+    $('#bet-confirm').click();
+}
+
+/**
+ *
  * @summary 获取投注号码
  * */
 function getInvestNumberString() {
-    var container = $('#investNumbersContainer');
+    var container = $('#' + investNumbersContainer);
     var investNumberString = container.text();
     if (investNumberString != '') {
         //清空号码
-        container.text();
-        //投注
-        ViewModel.textForm(investNumberString);
-        $('#bet-confirm').click();
+        viewModel.textForm('');
+        container.text('');
+        //后二投注
+        houErZhiXuanInvest(investNumberString);
     } else {
         return;
     }
@@ -21,9 +47,9 @@ function getInvestNumberString() {
  * 检查是否存在注入的元素节点
  * */
 $(function () {
-    var investNumbersContainer = $(document.body).find('#investNumbersContainer');
-    if (investNumbersContainer.length == 0) {
-        $(document.body).append('<div id="investNumbersContainer"></div>');
+    var container = $(document.body).find('#' + investNumbersContainer);
+    if (container.length == 0) {
+        $(document.body).append('<div id="' + investNumbersContainer + '"></div>');
     }
     setInterval(function () {
         getInvestNumberString();
